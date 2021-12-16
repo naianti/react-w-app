706660869979d68e8eeccd1b6d6f689a85954f54
naianti <87500932+naianti@users.noreply.github.com>
2021-12-16 07:22:35 -0400

    erased inncesary files and added boostrap

--- src/App.js
+++ src/App.js
@@ -14,87 +14,89 @@ export default function App() {
 
   return (
     <div className="App">
-      <header>
-        <h1>What's the weather in...</h1>
-      </header>
-      <div className="weather-app">
-        <section>
-          <form className="searcher">
-            <div className="row align-items-center">
-              <div className="col-6 col-l-6">
-                <input
-                  type="search"
-                  className="form-control"
-                  placeholder="Search any city here... (ex. Madrid, ES)  "
-                  autoComplete="off"
-                  autoFocus="on"
-                />
-              </div>
+      <div className="container">
+        <header>
+          <h1>What's the weather in...</h1>
+        </header>
+        <div className="weather-app">
+          <section>
+            <form className="searcher">
+              <div className="row align-items-center">
+                <div className="col-6 col-l-6">
+                  <input
+                    type="search"
+                    className="form-control"
+                    placeholder="Search any city here... (ex. Madrid, ES)  "
+                    autoComplete="off"
+                    autoFocus="on"
+                  />
+                </div>
 
-              <div className="col-6 col-l-4">
-                <input type="submit" className="button" value="Go!" />
-                <button className="button-location">My Location</button>
+                <div className="col-6 col-l-4">
+                  <input type="submit" className="button" value="Go!" />
+                  <button className="button-location">My Location</button>
+                </div>
+              </div>
+            </form>
+          </section>
+
+          <div className="row">
+            <div className="col-sm-5">
+              <div className="card-forecast-city">
+                <div className="card-body">
+                  <section>
+                    <h2>{weatherData.city + ", " + weatherData.country}</h2>
+                    <p>
+                      <img src="images/refresh.png" alt="refresh-icon" />
+                      Latest report:
+                    </p>
+
+                    <p>
+                      <span>
+                        <img src="images/calendar.png" alt="calendar-icon" />
+                        <span>{weatherData.date}</span>{" "}
+                      </span>
+                    </p>
+
+                    <p>
+                      <img src="images/stopwatch.png" alt="timer-icon" />
+                      <span>{weatherData.time}</span>
+                    </p>
+                  </section>
+                </div>
               </div>
             </div>
-          </form>
-        </section>
 
-        <div className="row">
-          <div className="col-sm-5">
-            <div className="card-forecast-city">
-              <div className="card-body">
-                <section>
-                  <h2>{weatherData.city + ", " + weatherData.country}</h2>
-                  <p>
-                    <img src="images/refresh.png" alt="refresh-icon" />
-                    Latest report:
-                  </p>
+            <div className="col-sm-7">
+              <div className="card-forecast-temp">
+                <div className="card-body">
+                  <section>
+                    <p>NOW</p>
+                    <span>{weatherData.temperature}</span>
+                    <span>°C</span>
 
-                  <p>
-                    <span>
-                      <img src="images/calendar.png" alt="calendar-icon" />
-                      <span>{weatherData.date}</span>{" "}
-                    </span>
-                  </p>
+                    <figure>
+                      <img src="./images/04n.png" alt="weather icon" />
+                    </figure>
 
-                  <p>
-                    <img src="images/stopwatch.png" alt="timer-icon" />
-                    <span>{weatherData.time}</span>
-                  </p>
-                </section>
+                    <h4>{weatherData.description}</h4>
+                    <p>
+                      Feels like <span>{weatherData.feelingTemp}</span> °C
+                    </p>
+                  </section>
+                </div>
               </div>
             </div>
           </div>
 
-          <div className="col-sm-7">
-            <div className="card-forecast-temp">
-              <div className="card-body">
-                <section>
-                  <p>NOW</p>
-                  <span>{weatherData.temperature}</span>
-                  <span>°C</span>
-
-                  <figure>
-                    <img src="./images/04n.png" alt="weather icon" />
-                  </figure>
-
-                  <h4>{weatherData.description}</h4>
-                  <p>
-                    Feels like <span>{weatherData.feelingTemp}</span> °C
-                  </p>
-                </section>
-              </div>
-            </div>
-          </div>
+          <section></section>
         </div>
-
-        <section></section>
+        <figure>
+          <div>
+            <div className="indicator"></div>
+          </div>
+        </figure>
       </div>
-      <figure>
-        <div>
-          <div className="indicator"></div>
-        </div>
-      </figure>
     </div>
   );
 }
