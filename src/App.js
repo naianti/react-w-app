854d13e00e0d8de9bd95af390f01ee428398e8a9
naianti <87500932+naianti@users.noreply.github.com>
2021-12-16 08:39:15 -0400

    Separate and update weather and app components

--- src/App.js
+++ src/App.js
@@ -1,101 +1,28 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
-  let weatherData = {
-    city: "Barquisimeto",
-    country: "VE",
-    date: "Saturday",
-    description: "Sunny",
-    time: "21:25",
-    temperature: "23",
-    feelingTemp: "22",
-  };
-
   return (
     <div className="App">
       <div className="container">
         <header>
           <h1>What's the weather in...</h1>
         </header>
-        <div className="weather-app">
-          <section>
-            <form className="searcher">
-              <div className="row align-items-center">
-                <div className="col-6 col-l-6">
-                  <input
-                    type="search"
-                    className="form-control"
-                    placeholder="Search any city here... (ex. Madrid, ES)  "
-                    autoComplete="off"
-                    autoFocus="on"
-                  />
-                </div>
+        <Weather />
 
-                <div className="col-6 col-l-4">
-                  <input type="submit" className="button" value="Go!" />
-                  <button className="button-location">My Location</button>
-                </div>
-              </div>
-            </form>
-          </section>
-
-          <div className="row">
-            <div className="col-sm-5">
-              <div className="card-forecast-city">
-                <div className="card-body">
-                  <section>
-                    <h2>{weatherData.city + ", " + weatherData.country}</h2>
-                    <p>
-                      <img src="images/refresh.png" alt="refresh-icon" />
-                      Latest report:
-                    </p>
-
-                    <p>
-                      <span>
-                        <img src="images/calendar.png" alt="calendar-icon" />
-                        <span>{weatherData.date}</span>{" "}
-                      </span>
-                    </p>
-
-                    <p>
-                      <img src="images/stopwatch.png" alt="timer-icon" />
-                      <span>{weatherData.time}</span>
-                    </p>
-                  </section>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-7">
-              <div className="card-forecast-temp">
-                <div className="card-body">
-                  <section>
-                    <p>NOW</p>
-                    <span>{weatherData.temperature}</span>
-                    <span>°C</span>
-
-                    <figure>
-                      <img src="./images/04n.png" alt="weather icon" />
-                    </figure>
-
-                    <h4>{weatherData.description}</h4>
-                    <p>
-                      Feels like <span>{weatherData.feelingTemp}</span> °C
-                    </p>
-                  </section>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <section></section>
-        </div>
-        <figure>
-          <div>
-            <div className="indicator"></div>
-          </div>
-        </figure>
+        <footer>
+          Coded by Nai Antillano and is{" "}
+          <a
+            href="https://github.com/naianti/react-w-app
+"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on Netlify
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,90 @@
 import React from "react";
-import axios from "axios";
+import "./Weather.css";
 
-export default function Weather(props) {
-  function showWeather(response) {
-    alert(
-      `The weather in ${response.data.name} is ${Math.round(
-        response.data.main.temp
-      )}°c`
-    );
-  }
-
-  let apiKey = "0c0f15195845da49d19b504381bfef7a";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+export default function Weather() {
+  let weatherData = {
+    city: "Barquisimeto",
+    country: "VE",
+    date: "Saturday",
+    description: "Sunny",
+    time: "21:25",
+    temperature: "23",
+    feelingTemp: "22",
+  };
 
   return (
     <div className="Weather">
-      <h2>This is react!</h2>
+      <section>
+        <form className="searcher">
+          <div className="row align-items-center">
+            <div className="col-6 col-l-6">
+              <input
+                type="search"
+                className="form-control"
+                placeholder="Enter any city here... (ex. Madrid, ES)  "
+                autoComplete="off"
+                autoFocus="on"
+              />
+            </div>
+
+            <div className="col-6 col-l-4">
+              <input type="submit" className="button" value="Go!" />
+              <button className="button-location">My Location</button>
+            </div>
+          </div>
+        </form>
+      </section>
+
+      <div className="row">
+        <div className="col-sm-5">
+          <div className="card-forecast-city">
+            <div className="card-body">
+              <section>
+                <h2>{weatherData.city + ", " + weatherData.country}</h2>
+                <p>
+                  <img src="images/refresh.png" alt="refresh icon" />
+                  Latest report:
+                </p>
+
+                <p>
+                  <span>
+                    <img src="images/calendar.png" alt="calendar icon" />
+                    <span>{weatherData.date}</span>{" "}
+                  </span>
+                </p>
+
+                <p>
+                  <img src="images/stopwatch.png" alt="timer icon" />
+                  <span>{weatherData.time}</span>
+                </p>
+              </section>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-sm-7">
+          <div className="card-forecast-temp">
+            <div className="card-body">
+              <section>
+                <p>NOW</p>
+                <span>{weatherData.temperature}</span>
+                <span>°C</span>
+
+                <figure>
+                  <img src="./images/04n.png" alt="weather icon" />
+                </figure>
+
+                <h4>{weatherData.description}</h4>
+                <p>
+                  Feels like <span>{weatherData.feelingTemp}</span> °C
+                </p>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section></section>
     </div>
   );
 }
