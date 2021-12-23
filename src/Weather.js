ab60bde5b08520f2c0cca8c24968f0e2085a1212
naianti <87500932+naianti@users.noreply.github.com>
2021-12-23 00:09:26 -0400

    create a new component only for the weather info description

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
-import FormattedTime from "./FormattedTime";
+import WeatherDescription from "./WeatherDescription";
 
 export default function Weather(props) {
   const [ready, setReady] = useState(false);
@@ -46,66 +45,7 @@ export default function Weather(props) {
           </form>
         </section>
 
-        <div className="row">
-          <div className="col-sm-5">
-            <div className="card-forecast-city">
-              <div className="card-body">
-                <section>
-                  <h2>{weatherData.city + ", " + weatherData.country}</h2>
-                  <p>
-                    <img
-                      src="https://weatherapp-nai-project.netlify.app/images/refresh.png"
-                      alt="refresh icon"
-                    />
-                    <span>Latest report:</span>
-                  </p>
-
-                  <p>
-                    <img
-                      src="https://weatherapp-nai-project.netlify.app/images/calendar.png"
-                      alt="calendar icon"
-                    />
-                    <FormattedTime date={weatherData.date} />
-                  </p>
-
-                  <p>
-                    <img
-                      src="https://weatherapp-nai-project.netlify.app/images/stopwatch.png"
-                      alt="timer icon"
-                    />
-                    <FormattedDate date={weatherData.date} />
-                  </p>
-                </section>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-7">
-            <div className="card-forecast-temp">
-              <div className="card-body">
-                <section>
-                  <h4>NOW</h4>
-                  <span className="temperature">
-                    {Math.round(weatherData.temperature)} °C
-                  </span>
-
-                  <figure>
-                    <img
-                      width="90px"
-                      src={weatherData.icon}
-                      alt="weather icon"
-                    />
-                  </figure>
-
-                  <h5 className="text-capitalize">{weatherData.description}</h5>
-                  <p>
-                    Feels like{" "}
-                    <span>{Math.round(weatherData.feelingTemp)} °C</span>
-                  </p>
-                </section>
-              </div>
-            </div>
-          </div>
-        </div>
+        <WeatherDescription data={weatherData} />
       </div>
     );
   } else {
---
+++ src/WeatherDescription.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+import FormattedDate from "./FormattedDate";
+import FormattedTime from "./FormattedTime";
+
+export default function WeatherDescription(props) {
+  return (
+    <div className="row">
+      <div className="col-sm-5">
+        <div className="card-forecast-city">
+          <div className="card-body">
+            <section>
+              <h2>{props.data.city + ", " + props.data.country}</h2>
+              <p>
+                <img
+                  src="https://weatherapp-nai-project.netlify.app/images/refresh.png"
+                  alt="refresh icon"
+                />
+                <span>Latest report:</span>
+              </p>
+
+              <p>
+                <img
+                  src="https://weatherapp-nai-project.netlify.app/images/calendar.png"
+                  alt="calendar icon"
+                />
+                <FormattedTime date={props.data.date} />
+              </p>
+
+              <p>
+                <img
+                  src="https://weatherapp-nai-project.netlify.app/images/stopwatch.png"
+                  alt="timer icon"
+                />
+                <FormattedDate date={props.data.date} />
+              </p>
+            </section>
+          </div>
+        </div>
+      </div>
+      <div className="col-sm-7">
+        <div className="card-forecast-temp">
+          <div className="card-body">
+            <section>
+              <h4>NOW</h4>
+              <span className="temperature">
+                {Math.round(props.data.temperature)} °C
+              </span>
+
+              <figure>
+                <img width="90px" src={props.data.icon} alt="weather icon" />
+              </figure>
+
+              <h5 className="text-capitalize">{props.data.description}</h5>
+              <p>
+                Feels like <span>{Math.round(props.data.feelingTemp)} °C</span>
+              </p>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
